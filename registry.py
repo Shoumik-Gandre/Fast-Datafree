85132512929cb0d2d9da823611adc9880bbc9743
Shoumik-Gandre <shoumikgandre@gmail.com>
2023-07-14 17:48:40 -0700

    -

--- registry.py
+++ registry.py
@@ -99,6 +99,7 @@ def get_dataset(name: str, data_root: str='data', return_transform=False, split=
     data_root = os.path.expanduser( data_root )
 
     if name == 'mnist-3ch':
+        print("-----[][][][]-----")
         num_classes = 10
         train_transform = T.Compose([
             T.Grayscale(num_output_channels=3),
